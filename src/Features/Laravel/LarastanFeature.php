d3fc85a2ce01e8c70f5b4d2070027da1e289de9b
Norman Huth <norman@huth.it>
2024-06-21 21:54:03 +0200

    feat: add larastan

---
+++ src/Features/Laravel/LarastanFeature.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NormanHuth\Lura\Features\Laravel;
+
+use NormanHuth\Lura\Commands\InstallLaravelCommand;
+use NormanHuth\Lura\Contracts\AbstractFeature;
+use NormanHuth\Lura\Support\Package;
+
+class LarastanFeature extends AbstractFeature
+{
+    /**
+     * Determine the name of the feature.
+     */
+    public static function name(): string
+    {
+        return '[Code Analysis] Larastan';
+    }
+
+    /**
+     * Perform action after create project.
+     */
+    public static function afterCreateProject(InstallLaravelCommand $command): void
+    {
+        $command->storage->publish('templates/phpstan.neon.dist', 'phpstan.neon.dist');
+    }
+
+    /**
+     * Determine composer scripts for this feature.
+     *
+     * @return array{string, string|array}
+     */
+    public static function composerScripts(InstallLaravelCommand $command): array
+    {
+        return ['phpstan' => './vendor/bin/phpstan analyse'];
+    }
+
+    /**
+     * Determine composer requirements for this feature.
+     *
+     * @return array<\NormanHuth\Lura\Support\Package>
+     */
+    public static function addComposerDevRequirement(InstallLaravelCommand $command): array
+    {
+        return [
+            new Package('larastan/larastan', '^2.9'),
+        ];
+    }
+}
