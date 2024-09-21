03d6547d3b2697fdf56f0f61b432edcff7e68355
Norman Huth <norman@huth.it>
2024-09-21 13:13:32 +0200

    feat: extend global type ts

--- templates/types/global.d.ts
+++ templates/types/global.d.ts
@@ -1,22 +1,56 @@
-import { PageProps as InertiaPageProps } from '@inertiajs/core';
-import { AxiosInstance } from 'axios';
-import { route as ziggyRoute } from 'ziggy-js';
-import { PageProps as AppPageProps } from './';
+/* eslint-disable */
+// noinspection JSUnusedGlobalSymbols
+
+import { PageProps as InertiaPageProps } from '@inertiajs/core'
+import { AxiosInstance } from 'axios'
+import { PageProps as AppPageProps } from './'
+// import { route as ziggyRoute } from 'ziggy-js'
+// import { ILogger } from 'js-logger'
+// import Translator from '@norman-huth/translator-js'
+// const JsonTranslator = new Translator()
+// const __ = (key: string, replace = {}) => {
+//   return JsonTranslator.translator(key, replace)
+// }
+// const trans = (key: string, replace = {}) => {
+//   return JsonTranslator.translator(key, replace)
+// }
+// const trans_choice = (key, number, replace = {}) => {
+//   return JsonTranslator.trans_choice(key, number, replace)
+// }
 
 declare global {
-    interface Window {
-        axios: AxiosInstance;
-    }
+  interface Window {
+    axios: AxiosInstance
+    // Logger: ILogger
+    // trans: trans
+    // trans_choice: trans_choice
+    // Translator: JsonTranslator
+  }
 
-    var route: typeof ziggyRoute;
+  // let route: typeof ziggyRoute
+  let axios: AxiosInstance
+  // let Logger: ILogger
+  // let __ = function (key: string, replace = {}) {
+  //   return JsonTranslator.trans(key, replace)
+  // }
+  // let trans = function (key: string, replace = {}) {
+  //   return JsonTranslator.trans(key, replace)
+  // }
+  // let trans_choice = function (key, number, replace = {}) {
+  //   return JsonTranslator.trans_choice(key, number, replace)
+  // }
 }
 
 declare module 'vue' {
-    interface ComponentCustomProperties {
-        route: typeof ziggyRoute;
-    }
+  interface ComponentCustomProperties {
+    // route: typeof ziggyRoute
+    // __: typeof __
+    // trans: typeof trans
+    // trans_choice: typeof trans_choice
+    // Translator: typeof JsonTranslator
+  }
 }
 
 declare module '@inertiajs/core' {
-    interface PageProps extends InertiaPageProps, AppPageProps {}
+  interface PageProps extends InertiaPageProps, AppPageProps {}
 }
